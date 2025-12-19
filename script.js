5307987b99d3945b1b18489c1d18e1dbd997a5a8
XNOXtm <tusharamishra2006@gmail.com>
2025-12-19 22:44:56 +0530

    Completed the entire project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
@@ -17,33 +18,44 @@ function getHumanChoice() {
     return humanChoice.toLowerCase();
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-function playRound( humanChoice, ComputerChoice ) {
+function playRound( humanSelection, computerSelection ) {
     if (humanSelection === computerSelection) {
-
+        
         return `Draw! You both choose ${humanSelection}`;
-
+        
     } else if ( (computerSelection === "stone" && humanSelection === "paper") || (computerSelection === "paper" && humanSelection === "scissor") || (computerSelection === "scissor" && humanSelection === "stone") ) {
-
+        
         humanScore += 1;
         return `You won! ${humanSelection} beats ${computerSelection}`;
-
+        
     } else if ( (humanSelection === "stone" && computerSelection === "paper") || (humanSelection === "paper" && computerSelection === "scissor") || (humanSelection === "scissor" && computerSelection === "stone") ) {
-
+        
         computerScore += 1;
         return `!!You lost ${computerSelection} beats ${humanSelection}`;
-
+        
     } else {
         return "Enter a Valid Choice !!!";
     };
 }
 
 function playGame() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound( humanSelection, humanSelection ));
-    }
+    let roundCount = 1;
+    while (roundCount <= 5) {
+        console.log("Round " + roundCount + " :");
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        
+        console.log(playRound( humanSelection, computerSelection ));
+        roundCount++;
+    };
+    
+    if (humanScore > computerScore) {
+        console.log("Congratulations!! You won the overall game")
+    } else if (humanScore < computerScore) {
+        console.log("Sorry!! \n Unfortunetly you lost to computer")
+    } else {
+        console.log("Aww its a draw ")
+    };
 }
 
 playGame();
