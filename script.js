1b29c63c633b645919eed3abb562718f705105dd
XNOXtm <tusharamishra2006@gmail.com>
2025-12-19 13:13:15 +0530

    Create a function getHumanChoice

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function getComputerChoice(max) {
     return Math.floor(Math.random() * max);
 }
 
+function getHumanChoice() {
+    return humanChoice = prompt("Choose one:\n0 for rock\n1 for paper\n2 for scissor");
+}
+
+
+/*
+
+*/
