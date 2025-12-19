de13e9981591863abd7a72faf173e1dc1d75f3d9
XNOXtm <tusharamishra2006@gmail.com>
2025-12-19 14:25:19 +0530

    Completed the playRound function with conditionals

--- script.js
+++ script.js
@@ -18,7 +18,15 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound( humanChoice, ComputerChoice ) {
-
+    if (humanSelection === computerSelection) {
+        console.log(`Draw! You both choose ${humanSelection}`);
+    } else if ( (computerSelection === "stone" && humanSelection === "paper") || (computerSelection === "paper" && humanSelection === "scissor") || (computerSelection === "scissor" && humanSelection === "stone") ) {
+        console.log(`You won! ${humanSelection} beats ${computerSelection}`);
+        humanScore += 1;
+    } else {
+        console.log(`You lost. ${computerSelection} beats ${humanSelection}`);
+        computerScore += 1;
+    }
 }
 
 function playGame() {
@@ -31,6 +39,17 @@ const computerSelection = getComputerChoice();
 playRound(humanSelection, computerSelection);
 
 
-/* PSEUDOCODE
-
+/* 
+1. Take humanchoice (out of rock, paper, scissor).
+2. Get computerschoice 
+3. Compare both inputs:
+    if both rock || scissor || stone:
+        draw
+    else if (computerchoice = stone && humanchoice = paper) || (computerchoice = paper && humanchoice = scissor) || (computerchoice = scissor && humanchoice = stone) :
+        you won {humanchoice} beats {computchoice}
+        humanscore += 1
+    else :
+        you lost {computerchoice} beat {humanchoice}
+        computerscore += 1
+    
 */
